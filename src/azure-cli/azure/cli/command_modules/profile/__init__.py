ff319da105b0ee973a59a4355a510d1e9884cbb7
jiasli <4003950+jiasli@users.noreply.github.com>
2024-10-15 19:49:39 +0800

    sp-cert

--- src/azure-cli/azure/cli/command_modules/profile/__init__.py
+++ src/azure-cli/azure/cli/command_modules/profile/__init__.py
@@ -46,8 +46,8 @@ class ProfileCommandsLoader(AzCommandsLoader):
             c.argument('username', options_list=['--username', '-u'],
                        help='User name, service principal client ID, or managed identity ID.')
             c.argument('password', options_list=['--password', '-p'],
-                       help='Credentials like password for user, or for a service principal, provide client '
-                            'secret or a PEM file with key and public certificate. Will prompt if not given.')
+                       help="Password for user account, or client secret for service principal. Will prompt if not "
+                            "given.")
             c.argument('tenant', options_list=['--tenant', '-t'], validator=validate_tenant,
                        help='The Microsoft Entra tenant, must provide when using a service principal.')
             c.argument('scopes', options_list=['--scope'], nargs='+',
@@ -65,6 +65,7 @@ class ProfileCommandsLoader(AzCommandsLoader):
             # Service principal
             c.argument('service_principal', action='store_true',
                        help='Log in with a service principal.')
+            c.argument('certificate', help='A PEM file with key and public certificate.')
             c.argument('use_cert_sn_issuer', action='store_true',
                        help='Use Subject Name + Issuer (SN+I) authentication in order to support automatic '
                             'certificate rolls.')
