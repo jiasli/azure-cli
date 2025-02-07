a0556b894ca8b903b34d692fbbd99e5309db1ca0
jiasli <4003950+jiasli@users.noreply.github.com>
2025-02-07 15:17:09 +0800

    linter

--- src/azure-cli-core/azure/cli/core/auth/credential_adaptor.py
+++ src/azure-cli-core/azure/cli/core/auth/credential_adaptor.py
@@ -3,11 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-import requests
 from knack.log import get_logger
-from knack.util import CLIError
-
-from .util import resource_to_scopes
 
 logger = get_logger(__name__)
 
